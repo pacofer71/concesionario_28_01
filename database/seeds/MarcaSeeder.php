429a5189e3be44fcb587b5eef529926d2ac2933f
pacofer <pacofer71@gmail.com>
2020-01-28 10:14:56 +0100

    Seed Marcas

--- database/seeds/MarcaSeeder.php
+++ database/seeds/MarcaSeeder.php
@@ -11,6 +11,38 @@ class MarcaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
+        DB::table('marcas')->insert([
+            'nombre'=>'Seat',
+            'pais'=>'España'
+        ]);
+        DB::table('marcas')->insert([
+            'nombre'=>'Renault',
+            'pais'=>'Francia'
+        ]);
+        DB::table('marcas')->insert([
+            'nombre'=>'VolksWagen',
+            'pais'=>'Alemania'
+        ]);
+        DB::table('marcas')->insert([
+            'nombre'=>'Citroen',
+            'pais'=>'Francia'
+        ]);
+        DB::table('marcas')->insert([
+            'nombre'=>'Fiat',
+            'pais'=>'Italia'
+        ]);
+        DB::table('marcas')->insert([
+            'nombre'=>'Opel',
+            'pais'=>'Alemania'
+        ]);
+        DB::table('marcas')->insert([
+            'nombre'=>'Ford',
+            'pais'=>'USA'
+        ]);
+        DB::table('marcas')->insert([
+            'nombre'=>'Toyota',
+            'pais'=>'Japon'
+        ]);
     }
 }
